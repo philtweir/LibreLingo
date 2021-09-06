9bb9c48c0a134e8ee0df34cce038972ee92519ba
Phil Weir <phil.weir@flaxandteal.co.uk>
2021-09-06 20:39:02 +0100

    fix dependencies [irish-from-english]

---
+++ gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public', // path to public directory
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/philtweir/LibreLingo.git', // Update to point to your repository  
+        user: {
+            name: 'Phil Weir', // update to use your name
+            email: 'phil.weir@flaxandteal.co.uk' // Update to use your email
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
