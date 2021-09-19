deb5ed7668d0f245d2b74fb1d77d4d642d5cfc8d
Phil Weir <phil.weir@flaxandteal.co.uk>
2021-09-19 18:44:13 +0100

    add pwa manifest

---
+++ apps/web/src/manifest.js
@@ -0,0 +1,17 @@
+{
+    "short_name": "Bandraoi",
+    "name": "Bandraoi",
+    "start_url": "/LibreLingo",
+    "icons": [
+      {
+        "src": "logo.png",
+        "type": "image/png",
+        "sizes": "512x512"
+      }
+    ],
+    "background_color": "#112",
+    "display": "standalone",
+    "scope": "/",
+    "theme_color": "#112",
+    "description": "Experiment with LibreLingo and Carrot+Pepper art for learning Irish"
+}
