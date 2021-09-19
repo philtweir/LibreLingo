8422091e3da54a7d4913932bd3e343277611c527
Phil Weir <phil.weir@flaxandteal.co.uk>
2021-09-19 19:03:41 +0100

    make the completed button distinction clearer

--- apps/web/src/components/SkillButton.svelte
+++ apps/web/src/components/SkillButton.svelte
@@ -39,6 +39,7 @@
       const circum = 2 * Math.PI * 10
       if (!completed) {
           const completedLayer = svg.getElementById("complete")
+          console.log(completedLayer);
           completedLayer.classList.add("invisible")
       } else {
           for (let i = 1; i <= 5; i++) {
